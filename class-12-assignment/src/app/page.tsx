d637ae075766c4772d61914063f6acb0b98fce1e
Cesar <cesarestevez04@gmail.com>
2026-06-07 01:12:10 -0500

    develop class 12 assignment

---
+++ class-12-assignment/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AlbumData from './data/DataDisplay';
+
+const page = () => {
+  return (
+    <>
+      <div className='text-3xl font-bold'>Class 12 Assignment - Hitting API with React (Tanstack) Query</div>
+      <AlbumData />
+    </>
+  )
+}
+
+export default page
